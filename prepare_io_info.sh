9d043f51572f0168a3f0b870626392edcc27390e
Mattia Santoro <mattia.santoro@cnr.it>
2021-04-07 11:05:00 +0200

    try to fix io info

--- prepare_io_info.sh
+++ prepare_io_info.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo InputPath:	./Fmask_InData/$(ls $1 | grep MSIL1C) > $2
+echo InputPath:	./Fmask_InData/$(ls $1 | grep MSIL1C)/GRANULE/$(ls $1/*/GRANULE/ | grep L1C) > $2
 echo OutputPath:	./OutData >> $2
